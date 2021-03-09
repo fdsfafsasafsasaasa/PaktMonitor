c4ae569abf50ce11e5db124bc4916299bda794e3
thewarsawpakt <smallperson07@gmail.com>
2021-03-09 15:55:24 -0600

    no need for oauth 2

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ RUN apt install -y python3 python3-pip git
 
 RUN pip3 install pymongo flask flask_discord
 
-RUN git clone https://github.com/thewarsawpakt/PaktMonitor
+RUN git clone --depth 1 https://github.com/thewarsawpakt/PaktMonitor
 
 RUN cd PaktMonitor; python3 -m paktmonitor
--- paktmonitor/views.py
+++ paktmonitor/views.py
@@ -1,12 +1,9 @@
 from paktmonitor.client.routes import *
-from paktmonitor.oauth.routes import *
 from paktmonitor.api.routes import *
 from paktmonitor.client import client
-from paktmonitor.oauth import oauth
 from paktmonitor.api import api
 
 from paktmonitor import paktmonitor
 
 paktmonitor.register_blueprint(client)
-paktmonitor.register_blueprint(oauth)
 paktmonitor.register_blueprint(api)
