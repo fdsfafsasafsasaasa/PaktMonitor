a3d0ff0f724c8fb23506bb6b09cf3842c1cb546f
thewarsawpakt <smallperson07@gmail.com>
2021-03-16 18:13:23 -0500

    removed unused import

--- paktmonitor/__init__.py
+++ paktmonitor/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from json import load
-import os
 
 paktmonitor = Flask("paktmonitor", static_folder="static/")
 paktmonitor.secret_key = b"99754106633f94d350db34d548d6091a"
