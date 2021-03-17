1c565387d65235095d8d7cfefb13aca11ab10cfc
thewarsawpakt <smallperson07@gmail.com>
2021-03-16 20:55:52 -0500

    better things

--- run.py
+++ run.py
@@ -3,4 +3,8 @@ from paktmonitor import paktmonitor
 import sys
 
 if __name__ == "__main__":
-    paktmonitor.run("127.0.0.1", sys.argv[1])
+    try:
+        paktmonitor.run("127.0.0.1", sys.argv[1])
+    except IndexError: # there should be a neater way to do this
+        paktmonitor.run("127.0.0.1", 5000)
+    
